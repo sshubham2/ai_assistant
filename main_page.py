33be9985045c0f6fa6ac4b07215f9769aa5af72f
Shubehndu Shubham <s2.shubh2@gmail.com>
2024-09-16 05:31:52 +0530

    changed page layout to wide

--- main_page.py
+++ main_page.py
@@ -5,6 +5,6 @@ programming_language_expert = st.Page("chatbot.py", title="Programming Language 
 natural_languag_expert = st.Page("chatbot_nne.py", title="Natural Language Expert", icon="💬")
 
 pg = st.navigation([home_page, programming_language_expert, natural_languag_expert])
-st.set_page_config(page_title="AI Assistant", page_icon="🤖")
+st.set_page_config(page_title="AI Assistant", page_icon="🤖", layout='wide')
 
 pg.run()
